9090d4d136c9261b0390fc2982c23caa51c9e635
Anton Svetlov <ttoadd.feedback@gmail.com>
2025-11-28 02:48:53 +0300

    Auth router

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 import database.modules
 from database.db import Base, engine
-
+from routers.auth_router import router as auth_router
 
 app = FastAPI(title='Auth Service API')
+app.include_router(auth_router)
+
 
 Base.metadata.create_all(engine)
